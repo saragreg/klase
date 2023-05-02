f6d52c1b56c30b465989db1ebc52b357d7a8a2a1
saragreg <saragregil2@gmail.com>
2023-05-02 18:48:46 +0200

    primero

---
+++ app/src/main/java/com/example/tfg_profes/InfoProfes.java
@@ -0,0 +1,14 @@
+package com.example.tfg_profes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoProfes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_profes);
+    }
+}
